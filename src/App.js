5ae7dd3a98319d3cb32960f66be3f5139c8bb85c
sserges <agounkeserge@gmail.com>
2019-02-19 15:06:44 +0000

    Conditional rendering Practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import React, {Component} from "react"
-import Conditional from "./components/Conditional"
+import React from "react"
 
-class App extends Component {
+/*
+Challenge:
+
+Given a stateless functional component:
+1. Follow the steps necessary to add state to it,
+2. Have state keep track of whether the user is logged in or not
+3. Add a button that logs the user in/out
+    a. extra challenge - make the button display "log in" if they're not logged in and "log out" if they are
+4. Display text that says "Logged in" if the user is logged in, or "Logged out" if they're not.
+*/
+
+class App extends React.Component {
+
     constructor() {
-        super()
+        super();
         this.state = {
-            unreadMessages: [
-                "Call your mom!",
-                "New spam email available. All links are definitely safe to click."
-            ]
+            isLoggedIn: false,
         }
+        this.handleClick = this.handleClick.bind(this);
     }
-    
+
+    handleClick() {
+        this.setState(prevState => {
+            return {
+                isLoggedIn: !prevState.isLoggedIn
+            }
+        })
+    }
+
     render() {
+        let buttonText = this.state.isLoggedIn ? "LOG OUT" : "LOG IN";
+        let displayText = this.state.isLoggedIn ? "Logged in" : "Logged out";
         return (
             <div>
-                {
-                    this.state.unreadMessages.length > 0 && <h2>You have {this.state.unreadMessages.length} unread messages!</h2>
-                }
+                <button onClick={this.handleClick}>{ buttonText }</button>
+                <h1>{displayText}</h1>
             </div>
         )
     }
