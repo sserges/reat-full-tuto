2b551384a56a35b04dcdaf70950e507d511daaf3
sserges <agounkeserge@gmail.com>
2019-02-19 10:53:21 +0000

    Conditional rendering part 1

--- src/App.js
+++ src/App.js
@@ -1,66 +1,34 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
-
-// import MyInfo from './components/MyInfo';
-// import Header from './components/Header';
-// import MainContent from './components/MainContent';
-// import Footer from './components/Footer';
-// import TodoItem from './components/TodoItem';
-// import ContactCard from './components/ContactCard';
-// import Joke from './components/Joke';
-// import Product from './components/Product';
-
-// import products from "./constants/vschoolProducts";
-// import todosData from './constants/todosData';
-
-import './App.css';
-
-// const todoComponents = todosData.map(todo => <TodoItem key={todo.id} text={todo.text} completed={todo.completed} />)
+import React, {Component} from "react"
+import Conditional from "./components/Conditional"
 
 class App extends Component {
-
-  render() {
-    // const date = new Date();
-    return (
-      <div>
-            <Header username="Bill" />
-            <Greeting />
-        </div>
-    );
-  }
-}
-
-class Header extends Component {
-
-  render() {
-    return (
-      <header>
-        <p>Welcome, {this.props.username}!</p>
-      </header>
-    );
-  }
-}
-
-
-class Greeting extends Component {
-
-  render() {
-    const date = new Date()
-    const hours = date.getHours()
-    let timeOfDay
-    
-    if (hours < 12) {
-        timeOfDay = "morning"
-    } else if (hours >= 12 && hours < 17) {
-        timeOfDay = "afternoon"
-    } else {
-        timeOfDay = "night"
+    constructor() {
+        super()
+        this.state = {
+            isLoading: true
+        }
     }
-
-    return (
-      <h1>Good {timeOfDay} to you, sir or madam!</h1>
-    );
-  }
+    
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoading: false
+            })
+        }, 1500)
+    }
+    
+    render() {
+        return (
+            <div>
+                {
+                    this.state.isLoading ?
+                        <h1>Loading...</h1>
+                    :
+                        <Conditional isLoading={this.state.isLoading}/>
+                }
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
---
+++ src/components/Conditional.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Conditional({isLoading}) {
+    return <h1>Some cool stuff about Conditional rendering</h1>
+}
+
+export default Conditional
