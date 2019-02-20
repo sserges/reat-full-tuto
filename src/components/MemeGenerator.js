5147ba463a9d362000e05b0f8dc61957824b5fe4
sserges <agounkeserge@gmail.com>
2019-02-20 16:32:17 +0000

    Arrow function used to avoid binding

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -9,9 +9,6 @@ class MemeGenerator extends Component {
             randomImg: "http://i.imgflip.com/1bij.jpg",
             allMemeImgs: []
         }
-
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     componentDidMount() {
@@ -23,12 +20,12 @@ class MemeGenerator extends Component {
             })
     }
 
-    handleChange(event) {
+    handleChange = (event) => {
         const {name, value} = event.target
         this.setState({[name]: value})
     }
 
-    handleSubmit(event) {
+    handleSubmit = (event) => {
         event.preventDefault()
         const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length)
         const randMemeImg = this.state.allMemeImgs[randNum].url
