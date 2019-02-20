62d42bb5fc28e3a48a0bf48f95bc0cecad29f09e
sserges <agounkeserge@gmail.com>
2019-02-20 16:17:38 +0000

    Meme Generator Capstone Project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react"
 
-import Form from './components/FormContainer'
-
+import Header from './components/Header'
+import MemeGenerator from './components/MemeGenerator'
 
 function App() {
     return (
-        <Form />
+        <div>
+            <Header />
+            <MemeGenerator />
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,15 @@
-import React from 'react';
-
-import './Header.css';
+import React from 'react'
 
 function Header() {
-    const date = new Date(2018, 6, 31, 15);
-    const hours = date.getHours();
-    let timeOfDay;
-    const styles = {
-        fontSize: 30,
-    };
-
-    if (hours < 12) {
-        timeOfDay = "morning";
-        styles.color ="#04756F";
-    } else if (hours >= 12 && hours < 17) {
-        timeOfDay = "afternoon";
-        styles.color ="#2E1927";
-    } else {
-        timeOfDay = "night";
-        styles.color ="#090000";
-    }
-
-    
-
     return (
-        <header style={styles} className="navbar">Good {timeOfDay}</header>
-    );
+        <header>
+            <img 
+                src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+                alt="Problem?"
+            />
+            <p>Meme Generator</p>
+        </header>
+    )
 }
 
-export default Header;
+export default Header
